bf762fc30c1b59b9174a0b19e29e99579f2901e7
WALID ALRIFAI <78854936+alwalidalrifai@users.noreply.github.com>
2021-04-27 20:54:01 +0200

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module JavaFX11 {
-    requires java.desktop;
-    requires javafx.graphics;
-    requires javafx.fxml;
-    requires javafx.controls;
-    requires java.sql;
-   /* requires mysql.connector.java;*/ //when we start dealing with sql
-    opens hu.unideb.inf;
-}
